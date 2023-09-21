b5201bb7c1d8a999f72ad57817a9c0a30041a0ca
Suresh Kamat <sureshkamat108@gmail.com>
2023-09-21 22:34:31 +0530

    order route completed

--- backend/app.js
+++ backend/app.js
@@ -4,13 +4,17 @@ const errorMiddleware=require("./middleware/error")
 const cookieParser=require('cookie-parser');
 app.use(express.json());
 app.use(cookieParser());
-const product=require("./routes/productRoute")
 
+//ROute Imports
+const product=require("./routes/productRoute");
+const user=require('./routes/userRoute');
+const order=require('./routes/orderRoute');
 
 app.use("/api/v1",product);
-
-const user=require('./routes/userRoute')
 app.use("/api/v1",user);
+app.use("/api/v1",order);
+
+
 //Middleware for Error
 app.use(errorMiddleware);
 
